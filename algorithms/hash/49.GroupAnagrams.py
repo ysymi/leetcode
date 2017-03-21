69085e2c0a08d70def71c65627f345b6146e6ac1
ysymi <ff.2011@live.cn>
2017-03-21 23:11:49 +0800

    finish the group anagrams  using hash

---
+++ algorithms/hash/49.GroupAnagrams.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        ret = {}
+        for s in strs:
+            leader = ''.join(sorted(s))
+            if leader not in ret:
+                ret[leader] = []
+            ret[leader].append(s)
+
+        return [v for k,v in ret.items()]
+
+
+
